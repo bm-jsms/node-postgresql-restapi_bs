7e67742f888163c5aecbd0fbb9dc1364ba9ad5b7
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-01-06 14:47:44 -0500

    [up] app - main conf

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import router from './routes/users.routes.js';
+import morgan from 'morgan';
 import { PORT } from './config.js';
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
