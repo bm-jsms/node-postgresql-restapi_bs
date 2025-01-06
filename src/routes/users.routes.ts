755e7a90021e3313339345456dc7ce6e6595e0fd
Josué EmaxS <josueemaxsalazar04@gmail.com>
2025-01-06 14:42:04 -0500

    [add] app users routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import {
+	createUser,
+	deleteUser,
+	getUser,
+	getUsers,
+	updateUser,
+} from '../controllers/users.controllers.js';
 
 const router: Router = Router();
 
-router.get('/users', (req, res) => {
-	res.send('Get users');
-});
+router.get('/users', getUsers);
+router.get('/users/:id', getUser);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
